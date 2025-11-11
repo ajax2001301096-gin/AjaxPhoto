75944d2fca75d0404f7b69cfe576c0299d541133
ajax301096 <147789857+ajax301096@users.noreply.github.com>
2025-11-11 22:47:53 +0900

    葉の色を変更しました。

---
+++ static/js/base.js
@@ -0,0 +1,8 @@
+// Tạo cánh hoa động bằng JavaScript
+const container = document.getElementById('petalsContainer');
+const petalCount = 20;
+for (let i = 0; i < petalCount; i++) {
+    const petal = document.createElement('div');
+    petal.className = 'petal';
+    container.appendChild(petal);
+}
