0098a3a5fc27ba6f2fda88e17193f5e41760fa8f
ajax301096 <147789857+ajax301096@users.noreply.github.com>
2025-11-15 23:06:50 +0900

    ページロードが遅いのため、修正しています

--- main/views.py
+++ main/views.py
@@ -3,10 +3,20 @@ from django.views import View
 from photo.models import Photo
 from location.models import Location
 from collections import defaultdict
+from django.views.decorators.cache import cache_page
+
 class MainIndexView(View):
   def get(self,request):
     #ブログによって全部写真取得
-    photos = Photo.objects.select_related('blogPhoto').all().order_by('uploaded_at')
+    photos = Photo.objects.select_related(
+            'blogPhoto',           # Load blog cùng lúc
+            'blogPhoto__location'  # Load location luôn (tránh N+1)
+        ).filter(
+            blogPhoto__isnull=False  # Chỉ lấy photo có blog
+        ).order_by(
+            '-blogPhoto__update_date',  # Blog mới nhất
+            'uploaded_at'
+        )[:60] 
 
     #blog_idによる写真グループ
     photos_by_blog = defaultdict(list)
@@ -15,7 +25,7 @@ class MainIndexView(View):
         photos_by_blog[photo.blogPhoto.blog_id].append(photo)
 
     blog_groups = []
-    for location_id, photo_list in photos_by_blog.items():
+    for blog_id, photo_list in photos_by_blog.items():
       blog_groups.append({
         'location':photo_list[0].blogPhoto.location,
         'photos': photo_list,
@@ -27,4 +37,4 @@ class MainIndexView(View):
     }
     return render(request,'main/index.html',context)
   
-mainindex = MainIndexView.as_view()
+mainindex = cache_page(60 * 5)(MainIndexView.as_view())
